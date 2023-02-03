125823454eae5eb13409d7fced15857244b1ae0e
Merve Ozgul <merveozgulmerve@gmail.com>
2023-02-04 00:11:50 +0300

    m1

--- src/test/java/team06/pages/HomePage.java
+++ src/test/java/team06/pages/HomePage.java
@@ -5,15 +5,29 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import team06.utilities.Driver;
 
+<<<<<<< Updated upstream
 public class HomePage {
     public HomePage(){
 
         PageFactory.initElements(Driver.getDriver(), this);
     }
+=======
+
+public class HomePage {
+
+
+    public HomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+    @FindBy(xpath = "//input[@type='search']")
+    public WebElement search;
+>>>>>>> Stashed changes
 
     @FindBy(linkText = "Sign In")
     public WebElement signIn;
 
+<<<<<<< Updated upstream
 
     @FindBy(linkText = "My Account")
     public WebElement myAccount;
@@ -27,3 +41,19 @@ public class HomePage {
 
 }
 
+=======
+    @FindBy(xpath = "(//*[text()='My Account'])[2]")
+    public WebElement myAccount;
+
+
+
+
+
+
+
+
+
+
+
+}
+>>>>>>> Stashed changes
