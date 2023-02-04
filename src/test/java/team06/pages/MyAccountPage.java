4c03f91f1f9f453b5768afd131f629a9e244aa43
zahideAfacan <zahideafacan66@gmail.com>
2023-02-04 18:29:31 -0500

    zUS_11_TC01

--- src/test/java/team06/pages/MyAccountPage.java
+++ src/test/java/team06/pages/MyAccountPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import team06.utilities.Driver;
 
+import java.util.List;
+
 public class MyAccountPage {
 
     public MyAccountPage() {
@@ -13,6 +15,40 @@ public class MyAccountPage {
 
         @FindBy(linkText = "Addresses")
         public WebElement addresses;
+
+    @FindBy(linkText = "Sign Out")
+    public WebElement signOut;
+
+    @FindBy(xpath = "//div[@class='col-md-4 col-sm-6 col-12 m-b-md mb-4']")
+    public List<WebElement> iconBoxes;
+
+    @FindBy(linkText = "Orders")
+    public WebElement Orders;
+
+    @FindBy(linkText = "Store Manager")
+    public WebElement storeManager;
+
+    @FindBy(linkText = "Downloads")
+    public WebElement Downloads;
+
+    @FindBy(linkText = "Addresses")
+    public WebElement Addresses;
+
+    @FindBy(linkText = "Account details")
+    public WebElement AccountDetails;
+
+    @FindBy(linkText = "Wishlist")
+    public WebElement Wishlist;
+
+    @FindBy(linkText = "Support Tickets")
+    public WebElement supportTickets;
+
+    @FindBy(linkText = "Followings")
+    public WebElement Followings;
+
+    @FindBy(linkText = "Logout")
+    public WebElement Logout;
+
 
 
 
