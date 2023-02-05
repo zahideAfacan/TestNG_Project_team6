25751b9fd44a7d713fc52bb44b5050d2bdd0e1e4
Merve Ozgul <merveozgulmerve@gmail.com>
2023-02-05 22:09:15 +0300

    m03

--- src/test/java/team06/tests/US_04/US_04_TC01.java
+++ src/test/java/team06/tests/US_04/US_04_TC01.java
@@ -13,72 +13,72 @@ import static team06.utilities.JSUtils.clickElementByJS;
 import static team06.utilities.ReusableMethods.waitFor;
 
 public class US_04_TC01 {
-    @Test
-    public void US04_TC01Test(){
+        @Test
+        public void US04_TC01Test(){
 <<<<<<< HEAD
-        HomePage   homePage=new HomePage();
+                HomePage   homePage=new HomePage();
 =======
-        HomePage_m1 homePage_m1=new HomePage_m1();
+                HomePage_m1 homePage_m1=new HomePage_m1();
 >>>>>>> c36d0d6409b7b154d9c53bb9c8f76af71590b868
-        LoginPage loginPage=new LoginPage();
-        MyAccountPage myAccountPage=new MyAccountPage();
-        EditShippingAddressPage editShippingAddressPage=new EditShippingAddressPage();
-        AddressesPage addressesPage=new AddressesPage();
-        Driver.getDriver().get(ConfigReader.getProperty("url_allover_commerce"));
+                LoginPage loginPage=new LoginPage();
+                MyAccountPage myAccountPage=new MyAccountPage();
+                EditShippingAddressPage editShippingAddressPage=new EditShippingAddressPage();
+                AddressesPage addressesPage=new AddressesPage();
+                Driver.getDriver().get(ConfigReader.getProperty("url_allover_commerce"));
 
-        homePage_m1.signInHome.click();
-        loginPage.username.sendKeys(ConfigReader.getProperty("allover_commerce_username"));
-        loginPage.password.sendKeys(ConfigReader.getProperty("allover_commerce_password"));
-        loginPage.signInButton.click();
-        waitFor(3);
-
-
-        //I tried to scroll down to the bottom the page then tried to click the element but it did not work.
-        //After I waitFor(2) then it is worked :)
-        Actions actions=new Actions(Driver.getDriver());
-        //scroll the button of the homepage
-        actions.sendKeys(Keys.END).perform();
-        waitFor(2);
-        //locate my account and click
-      //  homePage_m1.myAccount.click();
-        waitFor(2);
-
-        //Note: Also we can click by using JSExecutor
-        //clickElementByJS(homePage.myAccount);
+                homePage_m1.signInHome.click();
+                loginPage.username.sendKeys(ConfigReader.getProperty("allover_commerce_username"));
+                loginPage.password.sendKeys(ConfigReader.getProperty("allover_commerce_password"));
+                loginPage.signInButton.click();
+                waitFor(3);
 
 
-        //locate Adresses and click
-        myAccountPage.addresses.click();
-        addressesPage.editShippingAddress.click();
-        waitFor(1);
+                //I tried to scroll down to the bottom the page then tried to click the element but it did not work.
+                //After I waitFor(2) then it is worked :)
+                Actions actions=new Actions(Driver.getDriver());
+                //scroll the button of the homepage
+                actions.sendKeys(Keys.END).perform();
+                waitFor(2);
+                //locate my account and click
+                //  homePage_m1.myAccount.click();
+                waitFor(2);
 
-        //enter first name
-        editShippingAddressPage.shippingfirstName.clear();
-        editShippingAddressPage.shippingfirstName.sendKeys("Gulcin");
-
-        //enter last name
-        editShippingAddressPage.shippinglastName.clear();
-        editShippingAddressPage.shippinglastName.sendKeys("Selcuk");
-
-        Select select=new Select(editShippingAddressPage.shippingCountryDropdown);
-        select.selectByVisibleText("Turkey");
-        waitFor(1);
-
-        editShippingAddressPage.shippingAddress.clear();
-        editShippingAddressPage.shippingAddress.sendKeys("13 Leeds Lane");
-
-        editShippingAddressPage.shippingPostcode.clear();
-        editShippingAddressPage.shippingPostcode.sendKeys("21420");
-
-        editShippingAddressPage.shippingCity.clear();
-        editShippingAddressPage.shippingCity.sendKeys("Merkez");
-
-        Select select1=new Select(editShippingAddressPage.shippingStateDropdown);
-        select1.selectByVisibleText("Yalova");
-        waitFor(2);
+                //Note: Also we can click by using JSExecutor
+                //clickElementByJS(homePage.myAccount);
 
 
+                //locate Adresses and click
+                myAccountPage.addresses.click();
+                addressesPage.editShippingAddress.click();
+                waitFor(1);
 
-    }
+                //enter first name
+                editShippingAddressPage.shippingfirstName.clear();
+                editShippingAddressPage.shippingfirstName.sendKeys("Gulcin");
+
+                //enter last name
+                editShippingAddressPage.shippinglastName.clear();
+                editShippingAddressPage.shippinglastName.sendKeys("Selcuk");
+
+                Select select=new Select(editShippingAddressPage.shippingCountryDropdown);
+                select.selectByVisibleText("Turkey");
+                waitFor(1);
+
+                editShippingAddressPage.shippingAddress.clear();
+                editShippingAddressPage.shippingAddress.sendKeys("13 Leeds Lane");
+
+                editShippingAddressPage.shippingPostcode.clear();
+                editShippingAddressPage.shippingPostcode.sendKeys("21420");
+
+                editShippingAddressPage.shippingCity.clear();
+                editShippingAddressPage.shippingCity.sendKeys("Merkez");
+
+                Select select1=new Select(editShippingAddressPage.shippingStateDropdown);
+                select1.selectByVisibleText("Yalova");
+                waitFor(2);
+
+
+
+        }
 
 }
